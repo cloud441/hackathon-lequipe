eb67f5161e388badf244bac9ef26389e25e02352
kevin.guillet <kevin.guillet@epita.fr>
2021-03-29 16:09:32 +0200

    [DATA]: add databse bin files and python scripts

---
+++ data/bin2csv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Compile all binary files into one csv database.
+
+def bin2csv():
+    # ls data
+    # put in []
+    # [key, pic_1, pic_2, ...]
+    # write_csv() into data/keys_freq.csv
+    # 80% train, 20% validation
