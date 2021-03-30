df5f70b3cfe0e93366e9c88210ea86d33334891d
vertebarbe <35434346+vertebarbe@users.noreply.github.com>
2021-03-30 19:18:47 +0200

    [OPTIONS] added option to predict password

--- hackathon/__init__.py
+++ hackathon/__init__.py
@@ -1,5 +1,7 @@
 import hackathon.DBManager.manager as db
 import hackathon.DataAnalyser.analyser as analyser
+import hackathon.PredictPassword.predictor as predictor
+from data.bin2csv import bin2df
 
 # Error output of confusion matrix invalid call
 confusion_msg = "You couldn't build your confusion matrix with loaded model because of random \
@@ -9,7 +11,7 @@ train/valid database spliting (confusion matrix could be on trained value)."
 ''' Entry point of the project '''
 def run(argv):
     if len(argv) != 2:
-        print("Error: Invalid nb of parameter, 1 (train or load) is required.")
+        print("Error: Invalid nb of parameter, 1 (train, load or detect) is required.")
         return
 
     db_manager = db.DbManager()
@@ -24,8 +26,32 @@ def run(argv):
         print("Classifier is loading the model ...")
         if not classifier.loadModel():
             return
+    
+    elif argv[1] == 'detect':
+
+        # Load classifier
+        if not classifier.loadModel():
+            print("Could not load classifier model.")
+            return
+
+        # Load pics_tables
+        bin_path = "./data/given_files/data/pics_LOGINMDP.bin"
+        df = bin2df(bin_path)
+        
+
+        predict = predictor.PasswordPredictor(classifier)
+        print("Searching password...")
+
+        password = predict.predictPassword(df)
+        if not password:
+            print("Could not find any password.")
+        else:
+            print("Found password: %s" %password)
+        return
+        
+
     else:
-        print("Error: Invalid parameter '%s'\nvalid parameter are 'train' and 'load'" %argv[1])
+        print("Error: Invalid parameter '%s'\nvalid parameter are 'train', 'load' and detect" %argv[1])
         return
 
     if argv[1] == 'load':
