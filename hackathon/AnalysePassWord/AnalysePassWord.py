fe0549e912ff19a85f6aca23cf849c0c75ad9c7a
MaximeLeherle <maximeleherle@gmail.com>
2021-03-31 11:53:49 +0200

    Update AnalysePassWord.py
    Correct bug in the main function:
    
    leter are upper all time bacause forget to add .lower() when no shift detect.
    
    And supp don't add 5 on i so whe we have suppe we have 'uppr' all time

--- hackathon/AnalysePassWord/AnalysePassWord.py
+++ hackathon/AnalysePassWord/AnalysePassWord.py
@@ -19,7 +19,10 @@ def analyse_next_lettre(string, index):
                 return string[index]
         else:
             next_5_char = string[index:index+5]
-            if (next_5_char == "SPACE"):
+            next_4_char = string[index:index+4]
+            if (next_4_char == "CTRL"):
+                return "CTRL"
+            elif (next_5_char == "SPACE"):
                 return " "
             elif (next_5_char == "SHIFT"):
                 return "Shift"
@@ -31,6 +34,9 @@ def analyse_next_lettre(string, index):
                 return "\n"
             else:
                 return string[index]
+
+def filtered_list(list, occurence):
+    return [i for i in list if i != occurence]
 
 class AnalysePassword():
 
@@ -53,6 +59,7 @@ class AnalysePassword():
             print("Error : you want the", n, "eme char of password but passsword have only", len(self.string_to_analyse), "char.")
             return -1
 
+
     # main function to analyse the password
     # 
     # return : a list of all possible password
@@ -61,6 +68,7 @@ class AnalysePassword():
         password_out = [""]
 
         shift = False
+        ctrl = False
 
         list_of_5_letter_key = ["\n","","Shift"," "]
         list_of_4_letter_key = ["CTRL"]
@@ -71,25 +79,34 @@ class AnalysePassword():
             
             if (next_letter == -2):
                 print("Error in analyse_the_password because we are out of range")
+            elif (next_letter == 'CTRL'):
+                if (ctrl == True):
+                    password_out.append("")
+                ctrl = True
+            elif (not ctrl):
+                temp = 0
             elif (next_letter == -1):
                 if (len(password_out[-1]) != 0):
-                    password_out[-1] = password_out[-1][0:-2]
+                    password_out[-1] = password_out[-1][0:-1]
             elif (next_letter == "Shift"):
                 shift = True
-            elif (next_letter == "ctrl" or next_letter == "\n"):
+            elif (next_letter == "\n"):
                 password_out.append("")
+                ctrl = False
             else:
                 if (shift):
                     password_out[-1] = password_out[-1] + next_letter.upper()
                 else:
-                    password_out[-1] = password_out[-1] + next_letter
+                    password_out[-1] = password_out[-1] + next_letter.lower()
+                shift = False
             
             if (next_letter in list_of_4_letter_key):
                 i += 4
             elif (next_letter in list_of_5_letter_key):
                 i += 5
+            elif (next_letter == -1):
+                i += 5
             else:
                 i += 1
         
         return password_out
-            
