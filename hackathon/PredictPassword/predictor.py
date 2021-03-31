e5084e0653047e2753f068151a5931587d490c17
kevin.guillet <kevin.guillet@epita.fr>
2021-03-31 12:20:29 +0200

    [PREDICTOR]: Update parsing on bin translation.

--- hackathon/PredictPassword/predictor.py
+++ hackathon/PredictPassword/predictor.py
@@ -11,28 +11,56 @@ class PasswordPredictor():
 
 
     ''' Read a flow of frame to build the secret password. '''
-    # idea: detect ENTER in the end
     def findPassword(self, frames_table, i):
         garbageKeys = ["NOKEY", "CTRL"]
         pwd = ""
+        enter_block_size = 0
+        cur_block_size = 1
+        stop_block_size = 0
+        last_key = "NOKEY"
 
         while i < frames_table.shape[0]:
             key = self.predictKey(frames_table.iloc[i])
 
             if key == "ENTER":
+                enter_block_size += 1
+            else:
+                enter_block_size = 0
+
+            if enter_block_size == 3:
                 return pwd
+
             if key not in garbageKeys:
-                pwd += key
+                if key != last_key:
+                    stop_block_size += 1
+
+                    if stop_block_size == 3:
+                        if cur_block_size > 3:
+                            pwd += last_key
+
+                        cur_block_size = 1
+                        last_key = key
+                        stop_block_size = 0
+
+                else:
+                    stop_block_size = 0
+                    cur_block_size += 1
 
             i += 1
 
         return pwd
 
 
-    ''' Read a flow of frame and skip until the required key frame. '''
+    ''' Read a flow of frame and skip until the required key frame block. '''
     def skipUntilKey(self, pics_table, i, key):
+        block_size = 0
         while i < pics_table.shape[0]:
             if self.predictKey(pics_table.iloc[i]) == key:
+                block_size += 1
+            else:
+                block_size = 0
+
+            if block_size == 3:
                 return i
             i += 1
 
@@ -40,12 +68,12 @@ class PasswordPredictor():
         return -1
 
 
-    ''' Skip the flow of frame until first CTRL frame, return the index. '''
+    ''' Skip the flow of frame until first CTRL frame block, return the index. '''
     def beginLogin(self, pics_table):
         return self.skipUntilKey(pics_table, 0, "CTRL")
 
 
-    ''' Skip the flow of frame until no key frame is detected, return the index. '''
+    ''' Skip the flow of frame until no key frame block is detected, return the index. '''
     def endLogin(self, pics_table, i):
         return self.skipUntilKey(pics_table, i, "NOKEY")
 
