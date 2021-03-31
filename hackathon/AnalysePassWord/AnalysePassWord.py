c821e8530b45ac3770cbf483efd16fb68ec048f9
MaximeLeherle <maximeleherle@gmail.com>
2021-03-31 10:54:17 +0200

    Update AnalysePassWord.py
    Add comment for class function

--- hackathon/AnalysePassWord/AnalysePassWord.py
+++ hackathon/AnalysePassWord/AnalysePassWord.py
@@ -34,16 +34,23 @@ def analyse_next_lettre(string, index):
 
 class AnalysePassword():
 
+    # init function of the class
     def __init__(self, string_to_analyse):
         self.string_to_analyse = string_to_analyse
 
+    # return the len of the string_to_analyse
     def len_of_password(self):
         return self.string_to_analyse
 
+    # retrun the neme char of the string
+    #
+    # -1 is the error code when we are out of range
     def char_n_of_password(self, n):
 
         if (n < len(self.string_to_analyse)):
             return self.string_to_analyse[n]
         else:
             print("Error : you want the", n, "eme char of password but passsword have only", len(self.string_to_analyse), "char.")
-            return -1
+            return -1    # main function to analyse the password
+    # 
+    # return : a list of all possible password
